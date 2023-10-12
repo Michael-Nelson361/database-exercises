182d353d4d4d7223ba582b420e6e93ec6fc7c0c1
Michael Nelson <zsc.michaeln@gmail.com>
2023-10-12 13:35:01 -0500

    Created where_advanced_exercises and initialized exercise questions

---
+++ where_advanced_exercises.sql
@@ -0,0 +1,35 @@
+-- 1. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. What is the employee number of the top three results?
+
+
+-- 2. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. What is the employee number of the top three results? Does it match the previous question?
+
+
+-- 3. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. What is the employee number of the top three results?
+
+
+-- 4. Find all unique last names that start with 'E'.
+
+
+-- 5. Find all unique last names that start or end with 'E'.
+
+
+-- 6. Find all unique last names that end with E, but does not start with E?
+
+
+-- 7. Find all unique last names that start and end with 'E'.
+
+
+-- 8. Find all current or previous employees hired in the 90s. Enter a comment with the top three employee numbers.
+
+
+-- 9. Find all current or previous employees born on Christmas. Enter a comment with the top three employee numbers.
+
+
+-- 10. Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with the top three employee numbers.
+
+
+-- 11. Find all unique last names that have a 'q' in their last name.
+
+
+-- 12. Find all unique last names that have a 'q' in their last name but not 'qu'.
+
