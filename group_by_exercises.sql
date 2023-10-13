98245a837fcc40e10b30d6a020f31065a974a96b
Michael Nelson <zsc.michaeln@gmail.com>
2023-10-13 16:59:57 -0500

    Began work on group_by_exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,20 +1,38 @@
 -- Create a new file named group_by_exercises.sql
-
+show databases;
+use employees;
+select database();
+show tables;
 
 -- In your script, use DISTINCT to find the unique titles in the titles table. 
 -- How many unique titles have there ever been? 
 -- Answer that in a comment in your SQL file.
-
+select count(distinct title) 'Title Count'
+from titles
+;
+-- There are 7 unique titles
 
 -- Write a query to find a list of all unique last names that start and end with 'E' using GROUP BY.
-
+select last_name
+from employees
+where last_name like 'e%e'
+group by last_name
+;
+-- Returns 5 rows: Erde, Eldridgel Etalle, Erie, and Erbe
 
 -- Write a query to to find all unique combinations of first and last names of all employees whose last names start and end with 'E'.
-
+select first_name,last_name
+from employees
+where last_name like 'e%e'
+group by last_name,first_name
+order by last_name
+;
 
 -- Write a query to find the unique last names with a 'q' but not 'qu'. 
 -- Include those names in a comment in your sql code.
-
+select last_name
+from employees
+;
 
 -- Add a COUNT() to your results for exercise 5 to find the number of employees with the same last name.
 
